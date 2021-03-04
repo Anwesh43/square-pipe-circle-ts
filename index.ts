56292fdb6ca5aea2621516c970d7b49ae4921f31
Anwesh43 <anweshthecool0@gmail.com>
2021-03-04 22:12:22 +0530

    adding fixes

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ class ScaleUtil {
         return Math.max(0, scale - i / n)
     }
     static divideScale(scale : number, i : number, n : number) : number {
-        return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 
     static sinify(scale : number) {
